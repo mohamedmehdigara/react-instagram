2944926590f2d39d50679e6f1dc27ead69cbc4a0
mohamedmehdigara <garamehdi57@yahoo.com>
2023-06-11 10:44:15 +0100

    added files

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -15,6 +15,38 @@ const Feed = () => {
       });
   }, []);
 
+  const handleLike = (postId) => {
+    // Send a request to like the post
+    axios.post(`https://api.example.com/posts/${postId}/like`)
+      .then(response => {
+        // Update the liked state of the post
+        setPosts(prevPosts =>
+          prevPosts.map(post =>
+            post.id === postId ? { ...post, liked: true } : post
+          )
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const handleComment = (postId, comment) => {
+    // Send a request to add a comment to the post
+    axios.post(`https://api.example.com/posts/${postId}/comments`, { comment })
+      .then(response => {
+        // Update the comments array of the post
+        setPosts(prevPosts =>
+          prevPosts.map(post =>
+            post.id === postId ? { ...post, comments: [...post.comments, response.data] } : post
+          )
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="feed">
       <h2>Feed</h2>
@@ -24,6 +56,15 @@ const Feed = () => {
             <h4>{post.title}</h4>
             <p>{post.body}</p>
             <p>Author: {post.author}</p>
+            <div className="actions">
+              <button onClick={() => handleLike(post.id)}>{post.liked ? 'Unlike' : 'Like'}</button>
+              <input type="text" placeholder="Add a comment" onChange={(e) => handleComment(post.id, e.target.value)} />
+            </div>
+            <div className="comments">
+              {post.comments.map(comment => (
+                <p key={comment.id}>{comment.body}</p>
+              ))}
+            </div>
           </div>
         ))}
       </div>
